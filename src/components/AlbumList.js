df8400da1769df8148b9171eb3e05fd9933f5af8
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-03 10:24:21 -0700

    axios call works to get albums info

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import axios from 'axios'
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List!!</Text>
-        </View>
-    )
+class AlbumList extends React.Component {
+    componentDidMount(){
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+        // console.log('will mount')
+    }
+    render(){
+        return (
+            <View>
+                <Text>Album List!!</Text>
+            </View>
+        )
+    }
 }
 
-export default AlbumList;
+
+export default AlbumList; 
